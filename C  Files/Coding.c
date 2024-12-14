16494959a2cf564163b8ca4edde6680b7037e375
aweyandt <aweyandt@charlotte.edu>
2024-12-13 20:09:13 -0500

    fix

--- C  Files/Coding.c
+++ C  Files/Coding.c
@@ -3,25 +3,32 @@
 
 void exit_status(int status) {
     if (status == 0) {
-        printf("File is displayed\n");
+        printf("Testfile workds\n");
     } else if (status == 1) {
-        printf("Error opening file\n");
+        printf("womp womp\n");
     }
 }
 
 int main() {
-    FILE *fptr; // Declare file pointer
+    FILE *file_pointer; // Declare file pointer
 
-    fptr = fopen("testfile.txt", "r"); // Opens file in read mode ("r")
+    file_pointer = fopen("testfile.txt", "r"); // Opens file in read mode ("r")
+
+    char data_read[70];
 
     // Check if file was opened successfully
-    if (fptr == NULL) {
+    if (file_pointer == NULL) {
         // If fopen failed (file not found or cannot be opened), print the error message
         exit_status(1); // Exit with status 1
     } else {
         // File opened successfully
         exit_status(0); // Exit with status 0
-        fclose(fptr); // Close the file after use
+
+        while(fgets(data_read, sizeof(data_read), file_pointer) != NULL){
+            printf("%s", data_read);
+
+
+        }
     }
 
     return 0;
